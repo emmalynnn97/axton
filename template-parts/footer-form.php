ff47b3183fb377ec63b6eb2ee651712474892bc8
Emma Lynn <emmalynnn97@gmail.com>
2021-04-09 14:04:16 -0700

    fix some form fields

--- template-parts/footer-form.php
+++ template-parts/footer-form.php
@@ -2,8 +2,8 @@
 			<span>Request A Quote</span>
 			<form>
 				<input name='name' id='name' type="text" placeholder='name'/>
-				<input name='email' id='email' type="email" placeholder='name'/>
-				<input name='textarea' id='textarea' type='textarea' placeholder='notes'/>
+				<input name='email' id='email' type="email" placeholder='email'/>
+				<textarea name='textarea' id='textarea' type='textarea' placeholder='notes'></textarea>
 				<input type="submit"/>
 			</form>
 		</div>
