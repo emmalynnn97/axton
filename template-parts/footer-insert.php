8c8e61bdc017b7e7d8a5a344c175892b76e4fdd3
Emma Lynn <emmalynnn97@gmail.com>
2021-04-13 11:55:56 -0700

    update footer insert links to target=_blank

--- template-parts/footer-insert.php
+++ template-parts/footer-insert.php
@@ -3,16 +3,16 @@
 	<a><img class='nut' src="/wp-content/uploads/2021/03/NUt.png" alt="nut"/>Contact</a>
 	<a><img class='nut' src="/wp-content/uploads/2021/03/NUt.png" alt="nut"/>Videos</a>
 	<div class="footer-social-icons">
-	<a href="https://www.facebook.com/AxtonFleetSystems1?ref=hl"><svg style="background:rgb(56,76,158);" xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-facebook" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="none" fill="white" stroke-linecap="round" stroke-linejoin="round">
+	<a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/AxtonFleetSystems1?ref=hl"><svg style="background:rgb(56,76,158);" xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-facebook" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="none" fill="white" stroke-linecap="round" stroke-linejoin="round">
   		<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
  		<path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
 	</svg></a>
-<a href="https://www.youtube.com/channel/UCNYJVKWHOM-PzSlCkXVm6Jw">	<svg style="background:rgb(254,44,56)" xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-youtube" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+<a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/channel/UCNYJVKWHOM-PzSlCkXVm6Jw">	<svg style="background:rgb(254,44,56)" xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-youtube" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
   <rect x="3" y="5" width="18" height="14" rx="4" />
   <path d="M10 9l5 3l-5 3z" />
 </svg></a>
-	<a href="https://www.linkedin.com/company/axton-truck-equipment?trk=tyah&trkInfo=clickedVertical%3Acompany%2Cidx%3A1-1-1%2CtarId%3A1437595248709%2Ctas%3Aaxton%20fleet"><svg style="background:rgb(6,91, 140);" xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-linkedin" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+	<a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/company/axton-truck-equipment?trk=tyah&trkInfo=clickedVertical%3Acompany%2Cidx%3A1-1-1%2CtarId%3A1437595248709%2Ctas%3Aaxton%20fleet"><svg style="background:rgb(6,91, 140);" xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-linkedin" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
   <rect x="4" y="4" width="16" height="16" rx="2" />
   <line x1="8" y1="11" x2="8" y2="16" />
