e2634ddd7fdbb1cb6ca0b5e21fa4252fb0ca7e37
Emma Lynn <emmalynnn97@gmail.com>
2021-04-14 15:24:53 -0700

    fix hero backdrop

--- template-parts/home-hero.php
+++ template-parts/home-hero.php
@@ -1,8 +1,6 @@
 <div class="home-section home-hero">
-	<div class="hero-overlay">
 		<div class="hero-content">
 		<?php echo the_field('hero_text'); ?>
-	</div>
 		<a href="#welcome" class="direction">
 		<img src="/wp-content/uploads/2021/04/Direction-1.png" alt="direction"/>
 		</a>
