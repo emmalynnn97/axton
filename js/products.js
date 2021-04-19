ace33a0a71ff4e0ed339adbaea6bd49dedc49248
Emma Lynn <emmalynnn97@gmail.com>
2021-04-19 14:48:59 -0700

    update responsiveness of carousel

--- js/products.js
+++ js/products.js
@@ -1,8 +1,39 @@
-var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 3,
-    spaceBetween: 75,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+function adjustProductSlider(){
+    if(window.innerWidth > 800){
+        swiper = new Swiper('.swiper-container', {
+            slidesPerView: 3,
+            spaceBetween: 75,
+            navigation: {
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev',
+            },
+          });
+    }
+    if(window.innerWidth < 800){
+        swiper = new Swiper('.swiper-container', {
+            slidesPerView: 2,
+            spaceBetween: 25,
+            navigation: {
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev',
+            },
+          });
+    }
+    if(window.innerWidth < 600){
+        swiper = new Swiper('.swiper-container', {
+            slidesPerView: 1,
+            spaceBetween: 10,
+            navigation: {
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev',
+            },
+          });
+    }
+}
+function addEventListeners(){
+    window.addEventListener('load',adjustProductSlider)
+    window.addEventListener('resize', ()=>{
+        setTimeout(adjustProductSlider,1000)
+    })
+}
+addEventListeners()
