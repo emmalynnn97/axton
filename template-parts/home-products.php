78669555d96691f508080f5f804b918446e5fd7f
Emma Lynn <emmalynnn97@gmail.com>
2021-04-19 13:06:32 -0700

    initial commit of slider code and seperate folder for js

--- template-parts/home-products.php
+++ template-parts/home-products.php
@@ -1,4 +1,16 @@
+
 <div class="home-section home-products">
 	
-	Product Slider
+	<div class="product-slider-wrapper">
+		<div class="product-slider">
+			<div class="product-item initial">item 1</div>
+			<div class="product-item">item 2</div>
+      <div class="product-item">item 3</div>
+
+			<div class="carousel-button-next"></div>
+			<div class="carousel-button-prev"></div>
+
+		</div>
+	</div>
 </div>
+<script src='/wp-content/themes/axtontruck/js/products.js' type="text/javascript"></script>
