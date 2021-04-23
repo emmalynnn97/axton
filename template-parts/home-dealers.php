e5f8e693d836ea324738e9f9154f401efdd46656
Emma Lynn <emmalynnn97@gmail.com>
2021-04-23 12:55:45 -0700

    remove 2nd direction arrow

--- template-parts/home-dealers.php
+++ template-parts/home-dealers.php
@@ -5,7 +5,4 @@
 	<div class="home-fleets">
 		<?php get_template_part('template-parts/content', 'home-fleets'); ?>
 	</div>
-	<a href="#industries" class="direction">
-		<img src="/wp-content/uploads/2021/04/Direction-1.png" alt="direction"/>
-	</a>
 </div>
