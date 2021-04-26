f1c3f7654df3d924d9acac154d1ff24fd9cd3c7e
Emma Lynn <emmalynnn97@gmail.com>
2021-04-26 09:59:46 -0700

    make template for products page

---
+++ templates/template-products.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template Name: Products
+ * Template Post Type: post, page
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since 1.0
+ */
+
+
+get_header();
+?>
+
+<?php get_template_part('template-parts/products', 'hero'); ?>
+
+<?php get_footer(); ?>
