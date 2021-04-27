84f375f1e6be499d5dc7aa7f21ca1521092158ea
Emma Lynn <emmalynnn97@gmail.com>
2021-04-27 13:44:18 -0700

    finish up basic layout, next is content

--- template-parts/products-hero-content.php
+++ template-parts/products-hero-content.php
@@ -1,3 +1,6 @@
     <h1>
         Axton Truck Equipment <span>Products</span>
     </h1>
+    <a href="#products" class="direction">
+		<img src="/wp-content/uploads/2021/04/Direction-1.png" alt="direction"/>
+	</a>
--- template-parts/products-product-card.php
+++ template-parts/products-product-card.php
@@ -1,9 +1,10 @@
 <div class="product-card">
-        <img src="" alt="gooseneck flatbed">
+        <img src="/wp-content/uploads/2021/04/Gooseneck_pic.png" alt="gooseneck flatbed">
         <h2>
             Gooseneck Flatbeds
         </h2>
         <p>
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero molestiae placeat ducimus est aliquam accusantium ex culpa reiciendis iusto. Nobis officiis similique dolore rerum maiores eveniet architecto impedit sunt assumenda?
         </p>
+        <a href="#">View This product</a>
     </div>
