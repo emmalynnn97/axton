0229654ae952f2916264ab913c8bf5df15db9d6e
Emma Lynn <emmalynnn97@gmail.com>
2021-04-27 13:22:37 -0700

    add in product card

--- template-parts/products-cards.php
+++ template-parts/products-cards.php
@@ -1,1 +1,10 @@
-Product Card Here
+<div class="products-cards-container">
+    
+    <?php get_template_part('template-parts/products', 'product-card'); ?>  
+    <?php get_template_part('template-parts/products', 'product-card'); ?>  
+    <?php get_template_part('template-parts/products', 'product-card'); ?>  
+    <?php get_template_part('template-parts/products', 'product-card'); ?>  
+    <?php get_template_part('template-parts/products', 'product-card'); ?>  
+    <?php get_template_part('template-parts/products', 'product-card'); ?>  
+    <?php get_template_part('template-parts/products', 'product-card'); ?>  
+</div>
---
+++ template-parts/products-product-card.php
@@ -0,0 +1,9 @@
+<div class="product-card">
+        <img src="" alt="gooseneck flatbed">
+        <h2>
+            Gooseneck Flatbeds
+        </h2>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero molestiae placeat ducimus est aliquam accusantium ex culpa reiciendis iusto. Nobis officiis similique dolore rerum maiores eveniet architecto impedit sunt assumenda?
+        </p>
+    </div>
