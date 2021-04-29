9ab77f06a8758d57f0606263e2f87e76ea10068e
Emma Lynn <emmalynnn97@gmail.com>
2021-04-29 14:40:03 -0700

    remove comment with direction

--- template-parts/home-industries.php
+++ template-parts/home-industries.php
@@ -3,7 +3,4 @@
 		Industries We Serve
 	</h2>
 	<?php get_template_part('template-parts/content', 'industries-grid'); ?>
-	<!--<a href="#map" class="direction">
-		<img src="/wp-content/uploads/2021/04/Direction.png" alt="direction"/>
-		</a>-->
 </div>
