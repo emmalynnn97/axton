e406e63a88c2d9dd628611f2c5d977cddfd07b86
Emma Lynn <emmalynnn97@gmail.com>
2021-04-30 12:43:48 -0700

    set up interior template

---
+++ templates/template-interior.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template Name: Interior
+ * Template Post Type: post, page
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since 1.0
+ */
+
+
+get_header();
+?>
+
+<?php get_template_part('template-parts/interior', 'hero'); ?>
+
+
+<?php get_footer(); ?>
