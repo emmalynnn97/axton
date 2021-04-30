11bf6539427f55900c268e5185d3e2dbad8967df
Emma Lynn <emmalynnn97@gmail.com>
2021-04-30 13:50:46 -0700

    hero more or less good

--- template-parts/interior-hero-content.php
+++ template-parts/interior-hero-content.php
@@ -1,3 +1,10 @@
+<?php
+$title = get_the_title(); // This must be!, because this is the return - the_title would be echo
+$title_array = explode(' ', $title);
+$first_word = $title_array[0];
+$second_word = $title_array[1];
+?>
 <h1>
-    <?php echo the_title() ?>
+    <?php echo $first_word; ?>
+    <span><?php echo $second_word; ?></span>
 </h1>
