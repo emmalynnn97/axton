4326570691d15e68735a3103308360f2dbbf819c
Emma Lynn <emmalynnn97@gmail.com>
2021-04-30 15:19:26 -0700

    add in quotes-inventory section

--- template-parts/interior-inventory.php
+++ template-parts/interior-inventory.php
@@ -1,1 +1,3 @@
-bye
+<h2>Search Dealer Inventory</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+<a href="" class="axton-inventory-btn">Start Search</a>
--- template-parts/interior-quote.php
+++ template-parts/interior-quote.php
@@ -1,1 +1,3 @@
-hi
+<h2>Request a Quote</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+<a href="" class="axton-quote-btn">Request A Quote</a>
