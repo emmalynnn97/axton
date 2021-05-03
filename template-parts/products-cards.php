4dbdd1b7d3567504ffef00b4c58a34b3c21e3dca
Emma Lynn <emmalynnn97@gmail.com>
2021-05-03 12:26:56 -0700

    update products category pages with content

--- template-parts/products-cards.php
+++ template-parts/products-cards.php
@@ -1,10 +1,10 @@
 <div class="products-cards-container">
     
-    <?php get_template_part('template-parts/products', 'product-card'); ?>  
-    <?php get_template_part('template-parts/products', 'product-card'); ?>  
-    <?php get_template_part('template-parts/products', 'product-card'); ?>  
-    <?php get_template_part('template-parts/products', 'product-card'); ?>  
-    <?php get_template_part('template-parts/products', 'product-card'); ?>  
-    <?php get_template_part('template-parts/products', 'product-card'); ?>  
-    <?php get_template_part('template-parts/products', 'product-card'); ?>  
+    <?php get_template_part('template-parts/products', 'product-card-gooseneck'); ?>  
+    <?php get_template_part('template-parts/products', 'product-card-platform'); ?>  
+    <?php get_template_part('template-parts/products', 'product-card-service'); ?>  
+    <?php get_template_part('template-parts/products', 'product-card-crane'); ?>  
+    <?php get_template_part('template-parts/products', 'product-card-industrial'); ?>  
+    <?php get_template_part('template-parts/products', 'product-card-dump'); ?>  
+    <?php get_template_part('template-parts/products', 'product-card-van'); ?>  
 </div>
--- template-parts/products-product-card-platform.php
+++ template-parts/products-product-card-platform.php
@@ -1,10 +1,10 @@
 <div class="product-card">
-        <img src="/wp-content/uploads/2021/04/Gooseneck_pic.png" alt="gooseneck flatbed">
+        <img src="/wp-content/uploads/2021/05/Platform_pic.png" alt="gooseneck flatbed">
         <h2>
-            Gooseneck Flatbeds
+            Platform Bodies
         </h2>
         <p>
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero molestiae placeat ducimus est aliquam accusantium ex culpa reiciendis iusto. Nobis officiis similique dolore rerum maiores eveniet architecto impedit sunt assumenda?
         </p>
-        <a href="/gooseneck-flatbeds">View This product</a>
+        <a>View This product</a>
     </div>
