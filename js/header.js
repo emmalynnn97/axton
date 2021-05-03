129b513c20f533ada0ba35a2baed98761ddef347
Emma Lynn <emmalynnn97@gmail.com>
2021-05-03 14:55:35 -0700

    turn nav functions into arrow functions

--- js/header.js
+++ js/header.js
@@ -1,18 +1,18 @@
 const menuItems = document.querySelectorAll('.menu-item');
 const logo = document.querySelector('.custom-logo');
 const shrinkOn = 100;
-function getDistY(){
+const getDistY = () => {
     return document.documentElement.scrollTop || window.pageYOffset;
 }
-function shrinkNav(items){
+const shrinkNav = items => {
     items.forEach(item=>item.style.height="72px");
     logo.style.width='125px';
 }
-function growNav(items){
+const growNav = items => {
     items.forEach(item=>item.style.height="110px");
     logo.style.width='175px';
 }
-function handleScroll(){
-    getDistY() > shrinkOn ? shrinkNav(menuItems) : growNav(menuItems)
+const handleScroll = () => {
+    getDistY() > shrinkOn ? shrinkNav(menuItems) : growNav(menuItems);
 }
-document.addEventListener("scroll", handleScroll)
+document.addEventListener("scroll", handleScroll);
