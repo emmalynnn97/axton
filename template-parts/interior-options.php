249482a79a1bc4ddeb6376c5321d28a5796808fc
Emma Lynn <emmalynnn97@gmail.com>
2021-05-04 09:44:43 -0700

    make product interior page responsive

--- template-parts/interior-options.php
+++ template-parts/interior-options.php
@@ -3,4 +3,20 @@
         <?php get_template_part('template-parts/interior', 'option'); ?>
         <?php get_template_part('template-parts/interior', 'option'); ?>
     </div>
+    <div class="row">
+        <?php get_template_part('template-parts/interior', 'option'); ?>
+        <?php get_template_part('template-parts/interior', 'option'); ?>
+    </div>
+    <div class="row">
+        <?php get_template_part('template-parts/interior', 'option'); ?>
+        <?php get_template_part('template-parts/interior', 'option'); ?>
+    </div>
+    <div class="row">
+        <?php get_template_part('template-parts/interior', 'option'); ?>
+        <?php get_template_part('template-parts/interior', 'option'); ?>
+    </div>
+    <div class="row">
+        <?php get_template_part('template-parts/interior', 'option'); ?>
+        <?php get_template_part('template-parts/interior', 'option'); ?>
+    </div>
 </div>
