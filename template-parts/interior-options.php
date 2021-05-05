66677180e892f846c80c3fef4c91c6478bb9653b
Emma Lynn <emmalynnn97@gmail.com>
2021-05-05 15:51:42 -0700

    change out number of items so you can tell it's changing pages

--- template-parts/interior-options.php
+++ template-parts/interior-options.php
@@ -28,38 +28,10 @@ Options
         <?php get_template_part('template-parts/interior', 'option'); ?>
         <?php get_template_part('template-parts/interior', 'option'); ?>
     </div>
-    <div class="row">
-        <?php get_template_part('template-parts/interior', 'option'); ?>
-        <?php get_template_part('template-parts/interior', 'option'); ?>
-    </div>
-    <div class="row">
-        <?php get_template_part('template-parts/interior', 'option'); ?>
-        <?php get_template_part('template-parts/interior', 'option'); ?>
-    </div>
-    <div class="row">
-        <?php get_template_part('template-parts/interior', 'option'); ?>
-        <?php get_template_part('template-parts/interior', 'option'); ?>
-    </div>
-    <div class="row">
-        <?php get_template_part('template-parts/interior', 'option'); ?>
-        <?php get_template_part('template-parts/interior', 'option'); ?>
-    </div>
 </div>
 
 <div class="interior-gallery-outer">
     Gallery
-    <div class="row">
-        <?php get_template_part('template-parts/interior', 'option'); ?>
-        <?php get_template_part('template-parts/interior', 'option'); ?>
-    </div>
-    <div class="row">
-        <?php get_template_part('template-parts/interior', 'option'); ?>
-        <?php get_template_part('template-parts/interior', 'option'); ?>
-    </div>
-    <div class="row">
-        <?php get_template_part('template-parts/interior', 'option'); ?>
-        <?php get_template_part('template-parts/interior', 'option'); ?>
-    </div>
     <div class="row">
         <?php get_template_part('template-parts/interior', 'option'); ?>
         <?php get_template_part('template-parts/interior', 'option'); ?>
