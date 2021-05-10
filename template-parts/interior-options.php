d3c9fff497647fd5855a2119ebb475fc10d0d0bf
Emma Lynn <emmalynnn97@gmail.com>
2021-05-10 14:31:19 -0700

    remove specs and gray behind request a quote

--- template-parts/interior-options.php
+++ template-parts/interior-options.php
@@ -22,14 +22,6 @@ Options
     </div>
 </div>
 
-<div class="interior-specs-outer">
-    Specs
-    <div class="row">
-        <?php get_template_part('template-parts/interior', 'option'); ?>
-        <?php get_template_part('template-parts/interior', 'option'); ?>
-    </div>
-</div>
-
 <div class="interior-gallery-outer">
     Gallery
     <div class="row">
