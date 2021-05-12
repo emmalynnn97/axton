f1e5cfbe29b286d270c257aa2c48d785541e34f0
Emma Lynn <emmalynnn97@gmail.com>
2021-05-12 12:26:47 -0700

    update hero to use feature image

--- template-parts/interior-hero.php
+++ template-parts/interior-hero.php
@@ -1,3 +1,8 @@
 <div class="interior-hero">
     <?php get_template_part('template-parts/interior', 'hero-content') ?>	
 </div>
+<style>
+	.interior-hero{
+		background-image:url('<?php echo get_the_post_thumbnail_url(); ?>') !important;
+	}
+</style>
