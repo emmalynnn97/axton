240ce2e5d3b757509c82d726f3fc59b83c91d82f
Emma Lynn <emmalynnn97@gmail.com>
2021-06-07 10:40:26 -0700

    update interior options to include ACF

--- template-parts/interior-options.php
+++ template-parts/interior-options.php
@@ -1,28 +1,10 @@
+
 <div class="interior-options-wrapper">
-<div class="interior-options-outer active">
-    <div class="row">
-        <?php get_template_part('template-parts/interior', 'option-RD'); ?>
-        <?php get_template_part('template-parts/interior', 'option-SK'); ?>
+    <div class="interior-options-outer active">
+        <?php the_field('options'); ?>
     </div>
-    <div class="row">
-        <?php get_template_part('template-parts/interior', 'option-TM'); ?>
-        <?php get_template_part('template-parts/interior', 'option-TMD'); ?>
-    </div>
-    <div class="row">
-        <?php get_template_part('template-parts/interior', 'option-ER'); ?>
-        <?php get_template_part('template-parts/interior', 'option-toolboxes'); ?>
-    </div>
-</div>
 
 <div class="interior-gallery-outer">
     Gallery
-    <div class="row">
-        <?php get_template_part('template-parts/interior', 'option'); ?>
-        <?php get_template_part('template-parts/interior', 'option'); ?>
-    </div>
-    <div class="row">
-        <?php get_template_part('template-parts/interior', 'option'); ?>
-        <?php get_template_part('template-parts/interior', 'option'); ?>
-    </div>
 </div>
 </div>
