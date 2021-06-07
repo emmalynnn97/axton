2646397f2c7a7057b53da8aa0167c2037b6b50a0
Emma Lynn <emmalynnn97@gmail.com>
2021-06-07 11:08:57 -0700

    update interior-options to have acf for gallery too

--- template-parts/interior-option-RD.php
+++
@@ -1,8 +0,0 @@
-<div class="interior-option">
-            <img src="/wp-content/uploads/2021/05/image_FPO.png" alt="tie down image"/>
-            <div>
-                <h3>RD Model</h3>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem ullam aspernatur fugiat dolor excepturi voluptate laboriosam sint! Numquam, quaerat, ipsa quo temporibus laboriosam velit a, fugiat corporis minima nam deleniti.</p>
-            </div>
-            
-</div>
--- template-parts/interior-options.php
+++ template-parts/interior-options.php
@@ -1,10 +1,9 @@
-
 <div class="interior-options-wrapper">
     <div class="interior-options-outer active">
         <?php the_field('options'); ?>
     </div>
 
 <div class="interior-gallery-outer">
-    Gallery
+    <?php the_field('gallery'); ?>
 </div>
 </div>
